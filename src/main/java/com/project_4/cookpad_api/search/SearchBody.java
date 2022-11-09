81b5ed2f79e8cf85a55c8c2ee462ea4992d60a05
VIETTELGROUP\os_gpcp_gpdn46 <duypd@tasc.vn>
2022-11-09 18:47:28 +0700

    seed post

--- src/main/java/com/project_4/cookpad_api/search/SearchBody.java
+++ src/main/java/com/project_4/cookpad_api/search/SearchBody.java
@@ -20,6 +20,7 @@ public class SearchBody {
     private String phone;
     private String nameProduct;
     private String price;
+    private String search;
     private String namePost;
     private String sort;
     private String start;
@@ -34,6 +35,7 @@ public class SearchBody {
             private String email;
             private String phone;
             private String nameProduct;
+            private String search;
             private String price;
             private String namePost;
             private String sort;
@@ -54,6 +56,10 @@ public class SearchBody {
 
             public SearchBodyBuilder withUsername(String username) {
                 this.username = username;
+                return this;
+            }
+            public SearchBodyBuilder withSearchBody(String search) {
+                this.search = search;
                 return this;
             }
 
@@ -117,6 +123,7 @@ public class SearchBody {
                 searchBody.setEmail(email);
                 searchBody.setFullName(fullName);
                 searchBody.setNamePost(namePost);
+                searchBody.setSearch(search);
                 searchBody.setPrice(price);
                 searchBody.setPhone(phone);
                 searchBody.setNameProduct(nameProduct);
--- src/main/java/com/project_4/cookpad_api/seeder/ApplicationSeeder.java
+++ src/main/java/com/project_4/cookpad_api/seeder/ApplicationSeeder.java
@@ -1,8 +1,6 @@
 package com.project_4.cookpad_api.seeder;
 
-import com.project_4.cookpad_api.repository.PostRepository;
-import com.project_4.cookpad_api.repository.ProductRepository;
-import com.project_4.cookpad_api.repository.UserRepository;
+import com.project_4.cookpad_api.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -19,10 +17,16 @@ public class ApplicationSeeder implements CommandLineRunner {
     @Autowired
     ProductRepository productRepository;
     @Autowired
+    CategoryRepository categoryRepository;
+    @Autowired
+    OriginRepository originRepository;
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
     PostRepository postRepository;
     @Autowired
     PostSeeder postSeeder;
-    boolean seed = true;
+    boolean seed = false;
 
     @Override
     public void run(String... args) throws Exception {
@@ -30,6 +34,9 @@ public class ApplicationSeeder implements CommandLineRunner {
             userRepository.deleteAll();
             productRepository.deleteAll();
             postRepository.deleteAll();
+            categoryRepository.deleteAll();
+            roleRepository.deleteAll();
+            originRepository.deleteAll();
             userSeeder.generate();
             productSeeder.generate();
             postSeeder.generate();
